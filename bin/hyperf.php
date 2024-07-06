d26bd639eee6de4e0c5fa5f7031860def74b5111
Albert Chen <albert.cht@gmail.com>
2024-07-06 12:08:31 +0800

    chore: replace ClassLoader for dotenv reload

--- bin/hyperf.php
+++ bin/hyperf.php
@@ -15,7 +15,7 @@ require BASE_PATH . '/vendor/autoload.php';
 
 // Self-called anonymous function that creates its own scope and keep the global namespace clean.
 (function () {
-    Hyperf\Di\ClassLoader::init();
+    SwooleTW\Hyperf\Foundation\Di\ClassLoader::init();
     /** @var Psr\Container\ContainerInterface $container */
     $container = require BASE_PATH . '/bootstrap/app.php';
 
