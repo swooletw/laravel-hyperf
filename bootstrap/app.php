f4d666cdb62a17eec0e7b43f06200c850b9ebf16
Albert Chen <albert.cht@gmail.com>
2024-07-10 00:59:59 +0800

    chore: remove providers loader

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ use Hyperf\Context\ApplicationContext;
 use SwooleTW\Hyperf\Container\Container;
 use SwooleTW\Hyperf\Container\DefinitionSourceFactory;
 use SwooleTW\Hyperf\Foundation\Console\Contracts\Kernel as KernelContract;
-use SwooleTW\Hyperf\Foundation\ProvidersLoader;
 
 /**
  * Initialize a dependency injection container that implemented PSR-11 and return the container.
@@ -23,8 +22,5 @@ $container = ApplicationContext::setContainer(
     new Container((new DefinitionSourceFactory())())
 );
 $container->define(KernelContract::class, Kernel::class);
-
-(new ProvidersLoader($container))
-    ->load();
 
 return $container;
