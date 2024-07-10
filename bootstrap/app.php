e5ec4f7a99bccf7de7c376fe842a8d7617ee4dd1
Albert Chen <albert.cht@gmail.com>
2024-07-10 22:25:54 +0800

    feat: update bootstrap/app.php for new application class

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,26 +1,37 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
-use App\Console\Kernel;
-use Hyperf\Context\ApplicationContext;
-use SwooleTW\Hyperf\Container\Container;
-use SwooleTW\Hyperf\Container\DefinitionSourceFactory;
-use SwooleTW\Hyperf\Foundation\Console\Contracts\Kernel as KernelContract;
 
-/**
- * Initialize a dependency injection container that implemented PSR-11 and return the container.
- */
-$container = ApplicationContext::setContainer(
-    new Container((new DefinitionSourceFactory())())
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel Hyperf application instance
+| which serves as the "glue" for all the components of Laravel Hyperf, and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new SwooleTW\Hyperf\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
-$container->define(KernelContract::class, Kernel::class);
 
-return $container;
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed.
+|
+*/
+
+$app->define(
+    SwooleTW\Hyperf\Foundation\Console\Contracts\Kernel::class,
+    App\Console\Kernel::class
+);
+
+Hyperf\Context\ApplicationContext::setContainer($app);
+
+return $app;
