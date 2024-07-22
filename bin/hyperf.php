50e8c9de8f3cf8e58f9c1a53fe7435ebeceb5bca
Albert Chen <albert.cht@gmail.com>
2024-07-22 09:50:15 +0800

    fix: Fixed bug that default option cannot be loaded.

--- bin/hyperf.php
+++ bin/hyperf.php
@@ -6,12 +6,12 @@ ini_set('display_startup_errors', 'on');
 ini_set('memory_limit', '1G');
 
 error_reporting(E_ALL);
-date_default_timezone_set('Asia/Taipei');
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
-! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
 
 require BASE_PATH . '/vendor/autoload.php';
+
+! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
 
 // Self-called anonymous function that creates its own scope and keep the global namespace clean.
 (function () {
