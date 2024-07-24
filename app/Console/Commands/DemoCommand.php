5eafe02e381dd3c48b71ba34657770fe3e063ce7
Albert Chen <albert.cht@gmail.com>
2024-07-25 00:11:49 +0800

    chore: delete redundant code in DemoCommand

--- app/Console/Commands/DemoCommand.php
+++ app/Console/Commands/DemoCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use Psr\Container\ContainerInterface;
 use SwooleTW\Hyperf\Foundation\Console\Command;
 
 class DemoCommand extends Command
@@ -12,18 +11,8 @@ class DemoCommand extends Command
     protected ?string $signature = 'demo:hi {--name= : name}';
     protected string $description = 'Hyperf Demo Command';
 
-    public function __construct(protected ContainerInterface $container)
-    {
-        parent::__construct();
-    }
-
-    public function configure()
-    {
-        parent::configure();
-    }
-
     public function handle()
     {
-        $this->line('Hello ' . ($this->option('name') ?: 'Hyperf'), 'info');
+        $this->info('Hello ' . ($this->option('name') ?: 'Hyperf'));
     }
 }
