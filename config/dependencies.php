d5a86fafbc8408c795f422bfde197202964a0ca6
Albert Chen <albert.cht@gmail.com>
2024-07-24 12:41:16 +0800

    chore: update config/dependencies.php

--- config/dependencies.php
+++ config/dependencies.php
@@ -12,7 +12,4 @@ declare(strict_types=1);
 return [
     Hyperf\Logger\LoggerFactory::class => SwooleTW\Hyperf\Log\Adapter\LogFactoryAdapter::class,
     Hyperf\Validation\Middleware\ValidationMiddleware::class => App\Http\Middleware\ValidationMiddleware::class,
-    // Psr\EventDispatcher\EventDispatcherInterface::class => SwooleTW\Hyperf\Event\EventDispatcherFactory::class,
-    // Psr\EventDispatcher\ListenerProviderInterface::class => SwooleTW\Hyperf\Event\ListenerProviderFactory::class,
-    SwooleTW\Hyperf\Foundation\Console\Contracts\Kernel::class => App\Console\Kernel::class,
 ];
