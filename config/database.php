48e0c7d080c543a00d66c07ae20274b0750af5fe
Albert Chen <albert.cht@gmail.com>
2024-07-30 11:44:04 +0800

    adjust: migrate redis config to database config

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Hyperf\Stringable\Str;
+
 return [
 
     /*
@@ -66,7 +68,54 @@ return [
     |
     */
 
-    'migrations' => [
-        'table' => 'migrations',
+    'migrations' => 'migrations',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Redis Databases
+    |--------------------------------------------------------------------------
+    |
+    | Redis is an open source, fast, and advanced key-value store that also
+    | provides a richer body of commands than a typical key-value system
+    | such as Memcached. You may define your connection settings here.
+    |
+    */
+
+    'redis' => [
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'hyperf'), '_') . '_database_'),
+        ],
+
+        'default' => [
+            'host' => env('REDIS_HOST', 'localhost'),
+            'auth' => env('REDIS_AUTH', null),
+            'port' => (int) env('REDIS_PORT', 6379),
+            'db' => (int) env('REDIS_DB', 0),
+            'pool' => [
+                'min_connections' => 1,
+                'max_connections' => 10,
+                'connect_timeout' => 10.0,
+                'wait_timeout' => 3.0,
+                'heartbeat' => -1,
+                'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+            ],
+        ],
+
+        'queue' => [
+            'host' => env('REDIS_HOST', 'localhost'),
+            'auth' => env('REDIS_AUTH', null),
+            'port' => (int) env('REDIS_PORT', 6379),
+            'db' => (int) env('REDIS_DB', 0),
+            'pool' => [
+                'min_connections' => 1,
+                'max_connections' => 10,
+                'connect_timeout' => 10.0,
+                'wait_timeout' => 3.0,
+                'heartbeat' => -1,
+                'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+            ],
+        ],
     ],
 ];
