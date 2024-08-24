9e8e6fc1122963a2b40e632c10bb2248f56542e8
Albert Chen <albert.cht@gmail.com>
2024-08-25 00:06:01 +0800

    chore: keep AppServiceProvider empty

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,36 +4,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Hyperf\Contract\ConfigInterface;
-use SwooleTW\Hyperf\Support\Environment;
 use SwooleTW\Hyperf\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->addGracefulSignalHandlers();
+        //
     }
 
     public function register(): void
     {
-    }
-
-    protected function addGracefulSignalHandlers(): void
-    {
-        if (! $this->app->get(Environment::class)->isProduction()) {
-            return;
-        }
-
-        $handlers = $this->getConfig()->get('signal.handlers', []);
-        $this->getConfig()->set('signal.handlers', array_merge($handlers, [
-            \Hyperf\Process\Handler\ProcessStopHandler::class,
-            \SwooleTW\Hyperf\Foundation\Signal\WorkerStopHandler::class,
-        ]));
-    }
-
-    protected function getConfig(): ConfigInterface
-    {
-        return $this->app->get(ConfigInterface::class);
+        //
     }
 }
--- config/signal.php
+++ config/signal.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 return [
     'handlers' => [
+        // \SwooleTW\Hyperf\Foundation\Signal\WorkerStopHandler::class,
+        // \Hyperf\Process\Handler\ProcessStopHandler::class,
     ],
     'timeout' => 5.0,
 ];
