837919f2b8e17650ecef0b1576a4498909a223bc
Albert Chen <albert.cht@gmail.com>
2024-11-28 13:59:57 +0800

    chore: use runInCoroutines in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use SwooleTW\Hyperf\Foundation\Testing\Concerns\RunTestsInCoroutine;
 use SwooleTW\Hyperf\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RunTestsInCoroutine;
 }
