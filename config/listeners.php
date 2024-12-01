33b4b17f5b9bfd0af3c7a85cf985b863a5a391bc
Albert Chen <albert.cht@gmail.com>
2024-12-01 14:08:42 +0800

    chore: adjust default listeners config

--- config/listeners.php
+++ config/listeners.php
@@ -3,8 +3,5 @@
 declare(strict_types=1);
 
 return [
-    Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler::class,
-    Hyperf\Coordinator\Listener\ResumeExitCoordinatorListener::class,
-    SwooleTW\Hyperf\Foundation\Queue\Listeners\QueueHandleListener::class,
     // App\Listeners\DbQueryExecutedListener::class,
 ];
